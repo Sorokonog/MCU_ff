658e28753f6174653b03d2bdcf030be9c63f1899
Sorokonog <roman@voltbro.com>
2024-08-15 23:10:11 +0300

    comments added

--- Inc/main.h
+++ Inc/main.h
@@ -1,5 +1,21 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
 #define SOME_VALUE_OF_BITS_FOR_TRX_WAITING 90
 
+const std::chrono::nanoseconds UART_SIGNAL_INTERVAL{833333};
+const std::chrono::nanoseconds SIGNAL_DETECTION_INTERVAL{UART_SIGNAL_INTERVAL/10};
+
 bool waiting_transmit_to_start();
-bool signal_read(std::chrono::system_clock::time_point prev_reading, int * ret_val);
+bool is_time_for_signal_reading(std::chrono::system_clock::time_point prevReading);
 bool detect_signal_front_time_point(std::chrono::system_clock::time_point & signalFrontTimePoint, int signalLevel);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,69 +3,63 @@
 #include <bitset>
 #include <chrono>
 #include <cstdio>
+#include <vector>
 
 #include "main.h"
 
 #include "trackerinterface.h"
 
-using namespace std::chrono_literals;
+using namespace std::chrono_literals; //для возможностей использования литералов типа 1s, 5m и т.д.
 
-
-
-std::chrono::nanoseconds UART_SIGNAL_INTERVAL{833333};
-std::chrono::nanoseconds SIGNAL_DETECTION_INTERVAL{UART_SIGNAL_INTERVAL/10};
-
-//std::bitset<10> byte_to_fill_from_single_bits;
 
 int main(int, char**){
 
-
-
-    int retVal = 4;
-
-    //printf("%d\n", std::chrono::nanoseconds(UART_SIGNAL_INTERVAL_IN_MC).count());
+    int retVal;
     
-    waiting_transmit_to_start();
+    waiting_transmit_to_start(); //Ждем начала передачи пакета 
+    printf("0"); //0 для коррекции , это обусловлено алгоритмом работы функции waiting_transmit_to_start()
 
-    // std::chrono::system_clock::time_point signalToDetect;
-    // signalToDetect = std::chrono::system_clock::now();
-    // 
-    // printf("1st %d\n", std::chrono::nanoseconds(std::chrono::system_clock::now() - signalToDetect).count());
-    // detect_signal_front_time_point(signalToDetect, 1);
-    // printf("2nd %d\n", std::chrono::nanoseconds(signalToDetect - start).count());
-
-    std::chrono::system_clock::time_point readingTime{std::chrono::system_clock::now()};
+    std::chrono::system_clock::time_point prevReading{std::chrono::system_clock::now()};
     std::chrono::system_clock::time_point start{std::chrono::system_clock::now()};
 
-    while((start + std::chrono::seconds{2}) > std::chrono::system_clock::now())
+    //цикл вызывающий метод библиотеки get_signal_state()
+    while((start + 2s) > std::chrono::system_clock::now()) //читаем сигнал 2 секунды
     {
-        
-        if(signal_read(readingTime, &retVal))
+        if(is_time_for_signal_reading(prevReading))
         {
-            readingTime = std::chrono::system_clock::now();
-            printf("%d",retVal);
-        }
-        else
-        {
+            prevReading = std::chrono::system_clock::now();
+            retVal = get_signal_state();
+            printf("%d", retVal);
         }
     }
-    //printf("%d\n", std::chrono::nanoseconds(std::chrono::system_clock::now() - prev_reading).count());
 };
 
 
-bool signal_read(std::chrono::system_clock::time_point prevReading, int * retVal)
+/**
+ * @brief Функция проверяет пора ли читать следующий бит
+ *
+ * Частота чтения задается константой UART_SIGNAL_INTERVAL (1/1200 секунды)
+ * 
+ * @return true/false в зависимости от текущего и предыдущего момента чтения
+ */
+bool is_time_for_signal_reading(std::chrono::system_clock::time_point prevReading)
 {
-if((std::chrono::system_clock::now() - prevReading) > UART_SIGNAL_INTERVAL)
-{
-*retVal = get_signal_state();
-return true;
+    return((std::chrono::system_clock::now() - prevReading) > UART_SIGNAL_INTERVAL);
 }
-else
-{
-return false;
-}
-};
 
+
+/**
+ * @brief Функция актуализирует момент изменения фронта сигнала  
+ *
+ * @arg std::chrono::system_clock::time_point & signalFrontTimePoint - ссылка на временную метку начала фронта, int signalLevel уровень сигнала по которому мерить измнения
+ * 
+ * Потенциально данная функция может использоваться для синхронизации по стоповым битам
+ * Частота чтения задается константой SIGNAL_DETECTION_INTERVAL (1/12000 секунды)
+ * т.е. в 10 раз чаще чем обычная скорость UART. 
+ * Это позволяет точнее знать момент начала передачи следующего байта.
+ * 
+ * @return true/false в зависимости от успеха определения времени фронта
+ */
 bool detect_signal_front_time_point(std::chrono::system_clock::time_point & signalFrontTimePoint, int signalLevel)
 {
     bool signalFrontDetected = false;
@@ -74,7 +68,7 @@ bool detect_signal_front_time_point(std::chrono::system_clock::time_point & sign
     std::chrono::system_clock::time_point prevReading = std::chrono::system_clock::now();
     std::chrono::system_clock::time_point nowTimePoint;
 
-    while(!signalFrontDetected)
+    while(!signalFrontDetected) //потенциально можно использовать более сложную логику, но можно и заменить на просто true
     {
         nowTimePoint = std::chrono::system_clock::now();
         if((nowTimePoint - prevReading) > SIGNAL_DETECTION_INTERVAL)
@@ -98,6 +92,13 @@ bool detect_signal_front_time_point(std::chrono::system_clock::time_point & sign
     }
 }
 
+/**
+ * @brief Функция ждет начала передачи пакета отбрасывая единички в сигнале. 
+ *
+ * Блокирующая функция специально не дающая работать основному циклу, пока не началась передача посылки
+ * 
+ * @return true/false в зависимости от успеха определения начала посылки
+ */
 bool waiting_transmit_to_start()
 {
     int counter = 0;
@@ -105,17 +106,18 @@ bool waiting_transmit_to_start()
     bool txStarted = false;
     std::chrono::system_clock::time_point prevReading = std::chrono::system_clock::now();
     std::chrono::system_clock::time_point nowTimePoint;
-    while(!txStarted)
+    while(!txStarted) //потенциально можно использовать более сложную логику, но можно и заменить на просто true
     {
         nowTimePoint = std::chrono::system_clock::now();
-        if((nowTimePoint - prevReading) > SIGNAL_DETECTION_INTERVAL)
+        if((nowTimePoint - prevReading) > SIGNAL_DETECTION_INTERVAL) //Использую SIGNAL_DETECTION_INTERVAL, но можно и UART_SIGNAL_INTERVAL, есть мнение что с SIGNAL_DETECTION_INTERVAL результат получше
         {
             retVal = get_signal_state();    
         if(retVal)
         {
             counter++;   
         }
-        else if(!retVal && counter > SOME_VALUE_OF_BITS_FOR_TRX_WAITING)
+        //магическо эмприческое значение SOME_VALUE_OF_BITS_FOR_TRX_WAITING == 90. Если насчитали такое количество единиц подряд, значит мы в режиме ожидания.
+        else if(!retVal && counter > SOME_VALUE_OF_BITS_FOR_TRX_WAITING) 
         {
             return true;
         }
